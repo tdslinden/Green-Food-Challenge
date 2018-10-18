6e48df73dde3da103182e260f2c25a714825e3b3
Griffin Dominik Fung <griffinf@sfu.ca>
2018-10-17 22:27:32 -0700

    Implement basic page navigation for about section; Add text and images in about section; Add 2 image resources

--- app/src/main/java/com/android/greenfoodchallenge/carboncalculator/AboutActivity.java
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/AboutActivity.java
@@ -4,24 +4,83 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AboutActivity extends AppCompatActivity {
 
-    private Button mButtonMenu;
+    private int pageNumbers;
+    private int currentPageNumber = 0;
+    private List<Integer> pageIDList = new ArrayList<Integer>();
+    private List<Integer> navigationButtonIDList = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_savings);
+        setContentView(R.layout.activity_about);
+        populateIDList();
+        pageNumbers = pageIDList.size();
+        setupQuitButton();
+        for(int buttonID : navigationButtonIDList){
+            setupNavigationButton(buttonID);
+        }
+    }
 
-        mButtonMenu = (Button)findViewById(R.id.button_menu);
-        mButtonMenu.setOnClickListener(new View.OnClickListener() {
+    private void updatePage(int pageNumber){
+        setContentView(pageIDList.get(pageNumber));
+        setupQuitButton();
+        for(int buttonID : navigationButtonIDList){
+            setupNavigationButton(buttonID);
+        }
+    }
+
+    private void populateIDList(){
+        navigationButtonIDList.add(R.id.btnNextPage);
+        navigationButtonIDList.add(R.id.btnPreviousPage);
+        pageIDList.add(R.layout.activity_about);
+        pageIDList.add(R.layout.activity_about2);
+        pageIDList.add(R.layout.activity_about3);
+        pageIDList.add(R.layout.activity_about4);
+        pageIDList.add(R.layout.activity_about5);
+    }
+
+    private void setupNavigationButton(int buttonID){
+        Button button = (Button) findViewById(buttonID);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToMenu = new Intent(AboutActivity.this, MenuActivity.class);
-                startActivity(goToMenu);
+                int pressedButtonID = v.getId();
+                int newPageNumber = currentPageNumber;
+                if (pressedButtonID == R.id.btnNextPage){
+                    if (currentPageNumber < pageNumbers-1){
+                        newPageNumber += 1;
+                    }
+                }
+                else if (pressedButtonID == R.id.btnPreviousPage){
+                    if (currentPageNumber > 0){
+                        newPageNumber -= 1;
+                    }
+                }
+
+                Log.i("APPDEBUGLOG", Integer.toString(newPageNumber));
+                if (currentPageNumber != newPageNumber){
+                    currentPageNumber = newPageNumber;
+                    updatePage(currentPageNumber);
+                }
+            }
+        });
+    }
+
+    private void setupQuitButton(){
+        Button button = (Button) findViewById(R.id.btnQuit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
