ca738ac47f0e79e34ff6aeedc2529b00ac9ac8e1
Linden Kyaw <lkyaw@sfu.ca>
2018-10-21 10:54:15 -0700

    Added navigation arrow drawables and added back arrow to back button.

--- app/src/main/java/com/android/greenfoodchallenge/carboncalculator/CalcActivity.java
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/CalcActivity.java
@@ -33,9 +33,9 @@ public class CalcActivity extends AppCompatActivity {
                 number1 = inputNum1.getText().toString();
                 number2 = inputNum2.getText().toString();
                 number3 = inputNum3.getText().toString();
-                showToast(String.valueOf(number1));
-                showToast(String.valueOf(number2));
-                showToast(String.valueOf(number3));
+//                showToast(String.valueOf(number1));
+//                showToast(String.valueOf(number2));
+//                showToast(String.valueOf(number3));
 
                 ArrayList<String> listDouble = new ArrayList<String>();
                 listDouble.add(number1);
--- app/src/main/java/com/android/greenfoodchallenge/carboncalculator/SavingsActivity.java
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/SavingsActivity.java
@@ -9,20 +9,26 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-// After estimating the user’s current food carbon footprint, the carbon footprint calculator
-// should offer a way to see how much the user could save by changing the relative
-// proportions of their diet.
 
 public class SavingsActivity extends AppCompatActivity {
 
-    private Button mButtonMenu;
+    private double mCarbonFootprint = 12.34;
+    private String savedCarbonResultString;
+    private String formatSavedCarbonResultString;
+    private Button mBackButton;
     private static SeekBar seek_Bar;
     private static TextView text_view;
 
+    private MealPlans mMealPlan = new MealPlans(mCarbonFootprint);
+
     public void seekBar(){
         seek_Bar = (SeekBar)findViewById(R.id.seekBar);
-        text_view = (TextView)findViewById(R.id.textView);
-        text_view .setText("Coverage: " + seek_Bar.getProgress() + " / " + seek_Bar.getMax());
+        text_view = (TextView)findViewById(R.id.resultDescription);
+        mMealPlan.calculateSavedCarbonFootprint();
+        savedCarbonResultString = mMealPlan.doubleToString();
+        formatSavedCarbonResultString = getResources().getString(R.string.saving_calculator_result);
+        formatSavedCarbonResultString = String.format(formatSavedCarbonResultString, savedCarbonResultString);
+        text_view.setText(formatSavedCarbonResultString);
         seek_Bar.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
                     int progressValue;
@@ -30,21 +36,21 @@ public class SavingsActivity extends AppCompatActivity {
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                         progressValue = progress;
-                        text_view .setText("Coverage: " + progress + " / " + seek_Bar.getMax());
-                        Toast.makeText(SavingsActivity.this, "SeekBar in progress", Toast.LENGTH_LONG).show();
-
+                        mMealPlan.setMealPlan(progressValue);
+                        mMealPlan.calculateSavedCarbonFootprint();
+                        savedCarbonResultString = mMealPlan.doubleToString();
+                        formatSavedCarbonResultString = getResources().getString(R.string.saving_calculator_result);
+                        formatSavedCarbonResultString = String.format(formatSavedCarbonResultString, savedCarbonResultString);
+                        text_view.setText(formatSavedCarbonResultString);
                     }
 
                     @Override
                     public void onStartTrackingTouch(SeekBar seekBar) {
-                        Toast.makeText(SavingsActivity.this, "SeekBar in StartTracking", Toast.LENGTH_LONG).show();
 
                     }
 
                     @Override
                     public void onStopTrackingTouch(SeekBar seekBar) {
-                        text_view .setText("Coverage: " + progressValue + " / " + seek_Bar.getMax());
-                        Toast.makeText(SavingsActivity.this, "SeekBar in StopTracking", Toast.LENGTH_LONG).show();
 
                     }
                 }
@@ -57,13 +63,15 @@ public class SavingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_savings);
         seekBar();
 
-        mButtonMenu = (Button)findViewById(R.id.button_menu);
-        mButtonMenu.setOnClickListener(new View.OnClickListener() {
+        mBackButton = (Button)findViewById(R.id.backButton);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToMenu = new Intent(SavingsActivity.this, MenuActivity.class);
-                startActivity(goToMenu);
+                finish();
             }
         });
+
     }
+
+
 }
