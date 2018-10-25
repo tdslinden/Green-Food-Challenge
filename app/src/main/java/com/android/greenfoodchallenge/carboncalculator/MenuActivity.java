bfebea25851b1327d78826ebc59494fce0be8d5a
Griffin Dominik Fung <griffinf@sfu.ca>
2018-10-24 21:11:04 -0700

    Add menu resource, modify About and Menu page.
    Implement slideview for About Page as well as condense text to fewer pages.
    Modify About layouts to be more compact. Alter Menu appearance.

--- app/src/main/java/com/android/greenfoodchallenge/carboncalculator/MenuActivity.java
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/MenuActivity.java
@@ -3,7 +3,7 @@ package com.android.greenfoodchallenge.carboncalculator;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -37,7 +37,7 @@ public class MenuActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int pressedButtonID = v.getId();
                 if (pressedButtonID == R.id.btnAboutActivity){
-                    Intent intent = AboutActivity.makeIntent(MenuActivity.this);
+                    Intent intent = AboutSwipeActivity.makeIntent(MenuActivity.this);
                     startActivity(intent);
                 }
                 else if (pressedButtonID == R.id.btnCalculatorActivity){
