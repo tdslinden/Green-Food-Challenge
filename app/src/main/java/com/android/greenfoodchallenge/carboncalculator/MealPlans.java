b83cf48f8913fab4d51a60eca54acec4c33d8679
Linden Kyaw <lkyaw@sfu.ca>
2018-10-25 21:01:08 -0700

    Refactored and fixed code

--- app/src/main/java/com/android/greenfoodchallenge/carboncalculator/MealPlans.java
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/MealPlans.java
@@ -1,7 +1,5 @@
 package com.android.greenfoodchallenge.carboncalculator;
 
-import java.util.ArrayList;
-
 public class MealPlans {
     private static final double MEAT_EATER = 1.10;
     private static final double LOW_MEAT = 1.25;
@@ -12,37 +10,30 @@ public class MealPlans {
     private double mPotentialCarbonFootprint;
     private int mMealPlan;
     private String mPotentialCarbonFootprintString;
-    private ArrayList<String> userInputString;
-    private ArrayList<Double> userInputDouble;
 
 
     MealPlans (double carbonFootprint){
         mMealPlan = 0;
         mCarbonFootprint = carbonFootprint;
-        //userInputString = new ArrayList<>(userData);
     }
 
     public void calculateSavedCarbonFootprint(){
-        if(mPotentialCarbonFootprint != 0)
-        {
-            if(mMealPlan == 0){
-                mPotentialCarbonFootprint = mCarbonFootprint/MEAT_EATER;
-                mPotentialCarbonFootprint =  mCarbonFootprint - mPotentialCarbonFootprint;
-            }
-            else if(mMealPlan == 1){
-                mPotentialCarbonFootprint = mCarbonFootprint/LOW_MEAT;
-                mPotentialCarbonFootprint = mCarbonFootprint - mPotentialCarbonFootprint;
-            }
-            else if(mMealPlan == 2){
-                mPotentialCarbonFootprint = mCarbonFootprint/VEGETARIAN;
-                mPotentialCarbonFootprint = mCarbonFootprint - mPotentialCarbonFootprint;
-            }
-            else if(mMealPlan == 3){
-                mPotentialCarbonFootprint = mCarbonFootprint/VEGAN;
-                mPotentialCarbonFootprint = mCarbonFootprint - mPotentialCarbonFootprint;
-            }
+        if(mMealPlan == 0){
+            mPotentialCarbonFootprint = mCarbonFootprint/MEAT_EATER;
+            mPotentialCarbonFootprint =  mCarbonFootprint - mPotentialCarbonFootprint;
         }
-
+        else if(mMealPlan == 1){
+            mPotentialCarbonFootprint = mCarbonFootprint/LOW_MEAT;
+            mPotentialCarbonFootprint = mCarbonFootprint - mPotentialCarbonFootprint;
+        }
+        else if(mMealPlan == 2){
+            mPotentialCarbonFootprint = mCarbonFootprint/VEGETARIAN;
+            mPotentialCarbonFootprint = mCarbonFootprint - mPotentialCarbonFootprint;
+        }
+        else if(mMealPlan == 3){
+            mPotentialCarbonFootprint = mCarbonFootprint/VEGAN;
+            mPotentialCarbonFootprint = mCarbonFootprint - mPotentialCarbonFootprint;
+        }
     }
 
     public String doubleToString(){
@@ -66,5 +57,4 @@ public class MealPlans {
     public void setMealPlan(int mealPlan) {
         mMealPlan = mealPlan;
     }
-
 }
