7eeb106b0d13cad727b05b5866aa6512796d234e
ppa21 <ppa21@sfu.ca>
2018-11-11 20:07:40 -0800

    add meal and restaurant fields

--- app/src/main/java/com/android/greenfoodchallenge/carboncalculator/AddMeal.java
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/AddMeal.java
@@ -1,34 +1,21 @@
 package com.android.greenfoodchallenge.carboncalculator;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.Exclude;
 import com.google.firebase.database.FirebaseDatabase;
 
-
-import java.util.HashMap;
 import java.util.Map;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 public class AddMeal extends AppCompatActivity {
-
-    public static Intent makeIntent(Context context){
-        Intent intent =new Intent(context, AddMeal.class);
-        return intent;
-    }
-
     private DatabaseReference mDatabase;
     private EditText mealField;
     private EditText proteinField;
