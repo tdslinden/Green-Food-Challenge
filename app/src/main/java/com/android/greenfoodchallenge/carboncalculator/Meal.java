1ca58c76d3213987a5c09b08100fd59a4649c80a
Griffin Dominik Fung <griffinf@sfu.ca>
2018-11-13 23:09:50 -0800

    Add ViewMeal Activity.
    Create Meal class and expand Pledge class. Add ViewMeal activity and
    implement filter by city and protein. Add unit test for Meal class and
    update unit test for Pledge class.

---
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/Meal.java
@@ -0,0 +1,61 @@
+package com.android.greenfoodchallenge.carboncalculator;
+
+public class Meal {
+    private String Location;
+    private String Meal;
+    private String Protein;
+    private String Restaurant;
+
+    public Meal(){
+        Location = "Unknown";
+        Meal = "Unknown";
+        Protein = "Unknown";
+        Restaurant = "Unknown";
+    }
+
+    public Meal(String location, String meal, String protein, String restaurant) {
+        Location = location;
+        Meal = meal;
+        Protein = protein;
+        Restaurant = restaurant;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getMeal() {
+        return Meal;
+    }
+
+    public void setMeal(String meal) {
+        Meal = meal;
+    }
+
+    public String getProtein() {
+        return Protein;
+    }
+
+    public void setProtein(String protein) {
+        Protein = protein;
+    }
+
+    public String getRestaurant() {
+        return Restaurant;
+    }
+
+    public void setRestaurant(String restaurant) {
+        Restaurant = restaurant;
+    }
+
+    public boolean isValidMeal(){
+        if (Location.equals("Unknown") && Meal.equals("Unknown") && Protein.equals("Unknown") && Restaurant.equals("Unknown")){
+            return false;
+        }
+        return true;
+    }
+}
