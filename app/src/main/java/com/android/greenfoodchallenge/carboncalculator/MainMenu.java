dc9b828c75f3217f913d81b37205759300dd550c
mtfilo <41418306+mtfilo@users.noreply.github.com>
2018-11-15 22:59:40 -0800

    Still working on the map identifying your location (e.i. I'm at "X" restraunt)
    Currently using the calculator button at the bottom of the screen to send the user to the map while it's being worked on.

--- app/src/main/java/com/android/greenfoodchallenge/carboncalculator/MainMenu.java
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/MainMenu.java
@@ -53,7 +53,7 @@ public class MainMenu extends AppCompatActivity{
                             break;
 
                         case R.id.nav_calculator:
-                            Intent goToCalculator = mapActivity.makeIntent(MainMenu.this);
+                            Intent goToCalculator = MapsActivity.makeIntent(MainMenu.this);
                             startActivity(goToCalculator);
                             break;
 
