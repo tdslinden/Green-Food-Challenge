19ffc12af835d4d91347ce0dd6038c2c60551737
saritchi <saritchi@sfu.ca>
2018-11-20 16:27:27 -0800

    updated display page, with new text and pictures

--- app/src/main/java/com/android/greenfoodchallenge/carboncalculator/Display.java
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/Display.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.animation.Easing;
@@ -35,8 +36,8 @@ public class Display extends AppCompatActivity {
     ArrayList<String> data;
     double calories = 0;
 
-    TextView userFootprint;
-
+    TextView userFootprint, userCalories, mealResult;
+    ImageView resultPicture;
     Button mButtonSavings;
 
     //User Understanding
@@ -60,7 +61,11 @@ public class Display extends AppCompatActivity {
         setUpChart();
 
         // initialize the text views and buttons
+        userCalories = findViewById(R.id.display_calories);
+        userCalories.setText(toString().valueOf(calories));
         userFootprint = findViewById(R.id.user_footprint);
+        mealResult = findViewById(R.id.meal_comparison);
+        resultPicture = (ImageView)findViewById(R.id.car_picture);
         mButtonSavings = findViewById(R.id.button_to_savings);
         mButtonSavings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,15 +96,21 @@ public class Display extends AppCompatActivity {
         String startText;
         String endText;
         //UI text depends on if the userCO2e>typical CO2e
-        boolean isOver=calc.isOverAverage(footprint);
+        boolean isOver=calc.isOverAverage(footprint/1000);
 
         if(isOver){
             startText = res.getString(R.string.userUnderstandingTitleOverAverage);
             endText = res.getString(R.string.userUnderstandingOverAverage);
+            resultPicture.setImageResource(R.drawable.traffic);
+            double difference = (footprint/1000) - 1.5;
+            mealResult.setText(getString(R.string.mealCompare1) + String.format("%.2f", difference) + getString(R.string.mealCompare2) + getString(R.string.mealHigher) + getString(R.string.mealCompare3) + getString(R.string.overAverage));
+
         }
         else {
             startText = res.getString(R.string.userUnderstandingTitleUnderAverage);
             endText = res.getString(R.string.userUnderstandingUnderAverage);
+            double difference = 1.5 - (footprint/1000);
+            mealResult.setText(getString(R.string.mealCompare1) + String.format("%.2f", difference) + getString(R.string.mealCompare2) + getString(R.string.mealLower) + getString(R.string.mealCompare3) + getString(R.string.underAverage));
         }
 
         String resultText = res.getString(R.string.userUnderstandingTitle, startText, Integer.toString(calc.getCarEquivalence((footprint/1000))),endText);
