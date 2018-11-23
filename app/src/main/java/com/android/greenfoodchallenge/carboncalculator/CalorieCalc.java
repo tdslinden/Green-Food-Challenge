a18e723f964fc3469acdf59fffb7c7c3d359ceac
saritchi <saritchi@sfu.ca>
2018-11-22 19:28:35 -0800

    minor calorie calc fix

--- app/src/main/java/com/android/greenfoodchallenge/carboncalculator/CalorieCalc.java
+++ app/src/main/java/com/android/greenfoodchallenge/carboncalculator/CalorieCalc.java
@@ -223,7 +223,7 @@ public class CalorieCalc extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
                     disableConstraintFields(inputFields);
-
+                    inputFields.setVisibility(View.GONE);
                     changeFields.setText("Field View");
                     check1.setVisibility(VISIBLE);
                     check2.setVisibility(VISIBLE);
@@ -235,6 +235,7 @@ public class CalorieCalc extends AppCompatActivity {
 
                 } else {
                     enableConstraintFields(inputFields);
+                    inputFields.setVisibility(View.VISIBLE);
                     changeFields.setText("Input View");
                     check1.setVisibility(View.GONE);
                     check2.setVisibility(View.GONE);
